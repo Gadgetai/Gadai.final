76e7dc96ddf1d74f1c8253e21f2ad65d38ecd783
Gadgetai <gadget.ai.app@gmail.com>
2025-06-20 00:07:04 +0700

    translator_screen.dart

---
+++ lib/screens/translator_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TranslatorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Translator')),
+      body: Center(
+        child: Text('Translator Functionality Here'),
+      ),
+    );
+  }
+}
