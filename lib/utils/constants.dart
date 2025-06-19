027b85c7e4ba6e337efb3d8e1f316e22904e06df
Gadgetai <gadget.ai.app@gmail.com>
2025-06-20 00:32:46 +0700

    constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String appName = 'GAd Ai';
+  static const Duration animationDuration = Duration(milliseconds: 300);
+}
+
+class AppColors {
+  static const Color primary = Color(0xFF2D9CDB);
+  static const Color secondary = Color(0xFF56CCF2);
+  static const Color background = Color(0xFFF2F2F2);
+}
