5af789b1b78e714732eff5aac440eb1ab7d8738a
Gadgetai <gadget.ai.app@gmail.com>
2025-06-20 00:18:44 +0700

    feedback_screen.dart

---
+++ lib/screens/feedback_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeedbackScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Kritik & Saran")),
+      body: Center(
+        child: Text("Formulir Google diintegrasikan di sini."),
+      ),
+    );
+  }
+}
