f219ea36bf010116a8043d2fec1d9eeacd14771d
Gadgetai <gadget.ai.app@gmail.com>
2025-06-20 00:17:08 +0700

    candlestick_screen.dart

---
+++ lib/screens/candlestick_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CandlestickScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Analisa Candlestick")),
+      body: Center(
+        child: Text("Analisa grafik dan berita terkait crypto/forex."),
+      ),
+    );
+  }
+}
